d982e8f10d3979ee980542e728fdaf73173183dc
Tom <thomas.spencer@codurance.com>
2020-09-09 21:58:54 +0100

    Add not permit square to be played twice general

--- src/main/java/com/codurance/Board.java
+++ src/main/java/com/codurance/Board.java
@@ -1,22 +1,28 @@
 package com.codurance;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Board {
 
-  private Square takenSquare;
+  private Set<Square> takenSquares;
 
-  public Board(Square square) {
-    this.takenSquare = square;
+  public Board(Set<Square> squares) {
+    this.takenSquares = squares;
   }
 
   public Board() {
-    this.takenSquare = null;
+    this.takenSquares = Collections.emptySet();
   }
 
   public boolean alreadyTaken(Square square) {
-    return takenSquare == square;
+    return takenSquares.contains(square);
   }
 
   public Board take(Square square) {
-    return new Board(square);
+    Set<Square> newBoard = new HashSet<>(takenSquares);
+    newBoard.add(square);
+    return new Board(newBoard);
   }
 }
--- src/test/java/com/codurance/GameShould.java
+++ src/test/java/com/codurance/GameShould.java
@@ -3,6 +3,8 @@ package com.codurance;
 
 import static com.codurance.Player.O;
 import static com.codurance.Player.X;
+import static com.codurance.Square.TOP_LEFT;
+import static com.codurance.Square.TOP_MIDDLE;
 import static com.codurance.Status.GAME_ON;
 import static com.codurance.Status.SQUARE_ALREADY_PLAYED;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,23 +22,24 @@ public class GameShould {
   @Test
   void wait_for_O_to_play_after_X() {
     Game game = new Game();
-    game = game.play(Square.TOP_LEFT);
+    game = game.play(TOP_LEFT);
     assertThat(game.state()).isEqualTo(new GameState(GAME_ON, O));
   }
 
   @Test
   void alternate_the_players() {
     Game game = new Game();
-    game = game.play(Square.TOP_LEFT);
-    game = game.play(Square.TOP_MIDDLE);
+    game = game.play(TOP_LEFT);
+    game = game.play(TOP_MIDDLE);
     assertThat(game.state()).isEqualTo(new GameState(GAME_ON, X));
   }
 
   @Test
   void not_permit_square_to_be_played_twice() {
     Game game = new Game();
-    game = game.play(Square.TOP_LEFT);
-    game = game.play(Square.TOP_LEFT);
-    assertThat(game.state()).isEqualTo(new GameState(SQUARE_ALREADY_PLAYED, O));
+    game = game.play(TOP_LEFT);
+    game = game.play(TOP_MIDDLE);
+    game = game.play(TOP_LEFT);
+    assertThat(game.state()).isEqualTo(new GameState(SQUARE_ALREADY_PLAYED, X));
   }
 }
